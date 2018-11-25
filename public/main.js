a1709e37a19208b5b7e97f393c36b025cdb45dbd
Josh Feinstein <pillboxmediainc@gmail.com>
2018-11-25 02:10:59 -0500

    working prototype

--- public/main.js
+++ public/main.js
@@ -1,20 +1,21 @@
 // import React from 'react';
 // import readerApp from './readerApp';
+// import EventEmitter from 'events';
 
 function readerApp(string, speed) {
   let wordArray = string.split(`\n`).map(el => el.split(' '));
+  console.log(wordArray);
+  wordArray[wordArray.length - 1].push('&nbsp;');
   let mainDiv = document.getElementById('read-view');
   let k = 1;
 
   for (let i = 0; i < wordArray.length; i++) {
     for (let j = 1; j <= wordArray[i].length; j++) {
       setTimeout(() => {
-        if (wordArray[i].length < 2) {
+        if (wordArray[i].length < 2 && wordArray[i][0] === '') {
           alert('Click Ok to read the next paragraph');
         } else {
-          mainDiv.innerHTML = `<div class="read-text">${
-            wordArray[i][j - 1]
-          }</div>`;
+          mainDiv.innerHTML = `${wordArray[i][j - 1]}`;
         }
       }, k * speed);
       k++;
@@ -22,12 +23,12 @@ function readerApp(string, speed) {
   }
 }
 
-let myString = `This is paragraph one
+const button = document.getElementById('button');
+const textField = document.getElementById('text-input');
 
-This is paragraph two
+button.addEventListener('click', function() {
+  let inputText = textField.value;
+  let inputSpeed = document.getElementById('text-speed').value;
 
-This is paragraph three
-
-This is paragraph four`;
-
-readerApp(myString, 170);
+  readerApp(inputText, inputSpeed);
+});
